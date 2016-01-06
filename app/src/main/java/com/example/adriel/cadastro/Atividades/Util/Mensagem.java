a078b9eda59539be5f0deee26fc5f3e24d91adb4
adriel <adrielmalmsteen@msn.com>
2016-01-06 18:15:19 -0300

    Cadastro de Usuário

--- app/src/main/java/com/example/adriel/cadastro/Atividades/Util/Mensagem.java
+++
@@ -1,8 +0,0 @@
-package com.example.adriel.cadastro.Atividades.Util;
-
-/**
- * Created by adriel marques cavalcante on 06/01/16.
- */
-public class Mensagem {
-
-}
---
+++ app/src/main/java/com/example/adriel/cadastro/Util/Mensagem.java
@@ -0,0 +1,7 @@
+package com.example.adriel.cadastro.Util;
+
+/**
+ * Created by adriel on 06/01/16.
+ */
+public class Mensagem {
+}
